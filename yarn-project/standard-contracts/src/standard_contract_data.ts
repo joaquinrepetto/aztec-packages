cb788a154ccd9578c097d7432ab3b5ac1320cf1c
dbanks12 <david@aztec-labs.com>
2026-05-13 17:19:18 +0000

    feat(public_checks): demote to non-protocol contract; introduce hand-written interface

--- yarn-project/standard-contracts/src/standard_contract_data.ts
+++ yarn-project/standard-contracts/src/standard_contract_data.ts
@@ -3,20 +3,23 @@ import { Fr } from '@aztec/foundation/curves/bn254';
 import { FunctionSelector } from '@aztec/stdlib/abi';
 import { AztecAddress } from '@aztec/stdlib/aztec-address';
 
-export const standardContractNames = ['AuthRegistry'] as const;
+export const standardContractNames = ['AuthRegistry', 'PublicChecks'] as const;
 
 export type StandardContractName = (typeof standardContractNames)[number];
 
 export const StandardContractSalt: Record<StandardContractName, Fr> = {
   AuthRegistry: new Fr(1),
+  PublicChecks: new Fr(1),
 };
 
 export const StandardContractAddress: Record<StandardContractName, AztecAddress> = {
   AuthRegistry: AztecAddress.fromString('0x130fb3e9097456e6d3c486381ec788599d293fd5c0f178bc7e3724c0434faf42'),
+  PublicChecks: AztecAddress.fromString('0x0ed9aa9b8262bd0afb923764799e058ad10cf0de3e01dcf955055b11fdb20d70'),
 };
 
 export const StandardContractClassId: Record<StandardContractName, Fr> = {
   AuthRegistry: Fr.fromString('0x1bf882a1928c8e72e1e8e0e9595c721d5f5bae6466c24b39da40df04effdb1d7'),
+  PublicChecks: Fr.fromString('0x1853dc3b22d13d7fb93a089c366125098bf0f4a169255e12fc3765b3c08c3b6d'),
 };
 
 export const StandardContractClassIdPreimage: Record<
@@ -28,10 +31,16 @@ export const StandardContractClassIdPreimage: Record<
     privateFunctionsRoot: Fr.fromString('0x06e0363cea8d971d0c2988a13fc88774092b2858adc5ee0876ed2f9ad05e2f63'),
     publicBytecodeCommitment: Fr.fromString('0x2545f39893766508ce37bb5cea5e4dcab04c6f7f79f3089b1c076876e9d268b2'),
   },
+  PublicChecks: {
+    artifactHash: Fr.fromString('0x2b11692497c9a3f1f44f4694d1ffb1e521d8cd6ba0e2ef3878026fe9c12fd854'),
+    privateFunctionsRoot: Fr.fromString('0x202860adb1b8975971eeaf571aaaa88a27f4035290d58532ae7d60b0dfaad54c'),
+    publicBytecodeCommitment: Fr.fromString('0x013c4f854a5c87c9daf86c5f9bc07a42c2a061f1d924a5b3564ec7edc8e18cb7'),
+  },
 };
 
 export const StandardContractInitializationHash: Record<StandardContractName, Fr> = {
   AuthRegistry: Fr.fromString('0x0000000000000000000000000000000000000000000000000000000000000000'),
+  PublicChecks: Fr.fromString('0x0000000000000000000000000000000000000000000000000000000000000000'),
 };
 
 export const StandardContractPrivateFunctions: Record<
@@ -46,4 +55,5 @@ export const StandardContractPrivateFunctions: Record<
       vkHash: Fr.fromString('0x0e6dcc7becc25a6ee2e1322f2cc0ff6b59abff1bce27ca9906c079103e3d2c36'),
     },
   ],
+  PublicChecks: [],
 };
